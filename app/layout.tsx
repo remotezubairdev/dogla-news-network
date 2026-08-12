870e3853753309a5d526a344bbbcc0c0f5044d87
Zubair Dev <164741377+remotezubairdev@users.noreply.github.com>
2026-08-12 21:06:11 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,14 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
           {children}
-        </ThemeProvider>
       </body>
     </html>
   );
